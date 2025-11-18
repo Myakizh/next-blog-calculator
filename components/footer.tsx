526cd69c0f326cb7ba8a69775436074ec20a31ea
Myakizh <xoxlovvalentin5@gmail.com>
2025-11-19 01:12:28 +0200

    improve blog UI

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t mt-10">
+    <footer className="w-full border-t">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between py-4 text-sm text-gray-600">
         <span>© 2025 Blog Calculator Company</span>
 
